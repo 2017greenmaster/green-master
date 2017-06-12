faa66e87e15b9381756efa36f619c7e82c692feb
marcin <marcin@marcin>
2017-06-12 22:30:26 +0200

    - change constructor injection to annotation

--- src/main/java/bootwildfly/RankController.java
+++ src/main/java/bootwildfly/RankController.java
@@ -2,17 +2,18 @@ package bootwildfly;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/rank")
+@RestController("/rank")
 public class RankController {
 	
+	@Autowired
 	private RankService service;
 	
-	public RankController(RankService service) {
-		this.service = service;
+	public RankController() {
 	}
 	
     @RequestMapping(method = RequestMethod.PUT)
